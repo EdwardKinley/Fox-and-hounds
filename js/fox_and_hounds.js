dd95434a9dc02ecb859ee258f54811a6b829d06a
EdwardKinley <edward_kinley@yahoo.co.uk>
2019-07-31 00:46:41 +0100

    adds pieces to board

--- js/fox_and_hounds.js
+++ js/fox_and_hounds.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   const board = document.querySelector('.board');
+
   addBoard();
+  addPieces();
 
   function addBoard() {
     addRows();
@@ -35,6 +37,18 @@ document.addEventListener('DOMContentLoaded', () => {
         else if ((i+j)%2 == 1) { space.style.backgroundColor = 'black'; }
       }
     }
+  }
+
+  function addPieces() {
+    const hounds = [1, 3, 5, 7];
+    for (i=0; i<hounds.length; i++) {
+      const hound = document.createElement('div');
+      hound.className = 'hound';
+      document.querySelector(`#space0${hounds[i]}`).appendChild(hound);
+    }
+    const fox = document.createElement('div');
+    fox.className = 'fox';
+    document.querySelector('#space74').appendChild(fox);
   }
 
 })
