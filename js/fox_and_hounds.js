2ddabf464796907e45a2b91e505ac18f738b99e8
EdwardKinley <edward_kinley@yahoo.co.uk>
2019-07-31 00:30:57 +0100

    adds spaces to board

--- js/fox_and_hounds.js
+++ js/fox_and_hounds.js
@@ -23,7 +23,18 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   function addSpaces() {
-
+    for (i=0; i<8; i++) {
+      const row = document.querySelector(`#row${i}`);
+      for (j=0; j<8; j++) {
+        const space = document.createElement('div');
+        space.className = 'space';
+        space.id = `space${i}${j}`;
+        space.style.width = `${100/8}%`;
+        row.appendChild(space);
+        if ((i+j)%2 == 0) { space.style.backgroundColor = 'red'; }
+        else if ((i+j)%2 == 1) { space.style.backgroundColor = 'black'; }
+      }
+    }
   }
 
 })
